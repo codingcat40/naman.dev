41a7ba932de58c3c8e86d327aaf9f6e54cdbf045
Naman Matoliya <naman@fieldproxy.com>
2025-12-09 00:20:37 +0530

    component creation

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -2,64 +2,27 @@ export const projects = [
 
     {
         id: 1,
-        title: "project1",
-        description: "jasj jas jas jjsjas jaskf jdf jd fdj df kjsd j sdsjd su e kdlf kflc oodof sdwe cx",
-        stack: ["skill1", "skill2", "skill3", "skill4"]
+        title: "Forever - Full Stack Ecommerce Clothing store",
+        description: "Built a responsive full-stack clothing store using React, Node.js, Express, and MongoDB with complete user and admin flows. Implemented features like product sorting/filtering, keyword search, size selection, cart, checkout, and order management. Integrated secure user/admin authentication, Cloudinary for image uploads, and Stripe for online payments. Developed an admin dashboard with product/order CRUD, real-time order status updates, and toast notifications.",
+        stack: ["ReactJS", "Tailwind CSS", "Node", "Express", "JWT", "Stripe API"],
+        url: "https://e-commerce-clothingstore-eh52.vercel.app/"
     },
     {
         id: 2,
-        title: "project2",
-        description: "jasj jas jas jjsjas jaskf jdf jd fdj df kjsd j sdsjd su e kdlf kflc oodof sdwe cx",
-        stack: ["skill1", "skill2", "skill3", "skill4"]
+        title: "SwoleNORMOUS fitness app",
+        description: "A fitness application for fitness enthusiasts seeking proper workout plans. It offers personalized routines targeting individual muscles, bro splits, bodybuilder splits, and upper-lower body exercises. The app suggests appropriate plans for strength, cardio, and muscle gain goals, with detailed instructions, total reps, and rest times. App showcases proper training instructions and provides smooth and mobile responsive interface to the user.",
+        stack: ["React", "Tailwind CSS"],
+        url: "https://swolenormous-app.netlify.app/",
     },
 
     {
         id: 3,
-        title: "project3",
-        description: "jasj jas jas jjsjas jaskf jdf jd fdj df kjsd j sdsjd su e kdlf kflc oodof sdwe cx",
-        stack: ["skill1", "skill2", "skill3", "skill4"]
+        title: "MyBlog",
+        description: `A simple Blog Application built with the MERN stack (MongoDB, Express, React, Node.js).
+Users can register, log in, create posts, edit posts, and delete posts. Posts are displayed with pagination on the home page.
+`,
+        stack: ["ReactJS", "Node JS", "MongoDB", "JWT", "Express"],
+        url: "https://personal-blog2025.netlify.app/"
     },
 
-    {
-        id: 4,
-        title: "project4",
-        description: "jasj jas jas jjsjas jaskf jdf jd fdj df kjsd j sdsjd su e kdlf kflc oodof sdwe cx",
-        stack: ["skill1", "skill2", "skill3", "skill4"]
-    },
-
-    {
-        id: 5,
-        title: "project5",
-        description: "jasj jas jas jjsjas jaskf jdf jd fdj df kjsd j sdsjd su e kdlf kflc oodof sdwe cx",
-        stack: ["skill1", "skill2", "skill3", "skill4"]
-    },
-
-    {
-        id: 6,
-        title: "project6",
-        description: "jasj jas jas jjsjas jaskf jdf jd fdj df kjsd j sdsjd su e kdlf kflc oodof sdwe cx",
-        stack: ["skill1", "skill2", "skill3", "skill4"]
-    },
-
-
-    {
-        id: 7,
-        title: "project7",
-        description: "jasj jas jas jjsjas jaskf jdf jd fdj df kjsd j sdsjd su e kdlf kflc oodof sdwe cx",
-        stack: ["skill1", "skill2", "skill3", "skill4"]
-    },
-
-    {
-        id: 8,
-        title: "project8",
-        description: "jasj jas jas jjsjas jaskf jdf jd fdj df kjsd j sdsjd su e kdlf kflc oodof sdwe cx",
-        stack: ["skill1", "skill2", "skill3", "skill4"]
-    },
-
-    {
-        id: 9,
-        title: "project9",
-        description: "jasj jas jas jjsjas jaskf jdf jd fdj df kjsd j sdsjd su e kdlf kflc oodof sdwe cx",
-        stack: ["skill1", "skill2", "skill3", "skill4"]
-    },
 ]
--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -4,14 +4,19 @@ function Projects() {
   return (
     <div className="min-h-[calc(100vh-64px)] flex flex-col justify-between pt-24 pb-12 text-center gap-6">
       {projects.map((item) => {
-        return <div key={item.id} className="flex flex-col bg-gray-800 border gap-16 w-[50%] text-center m-auto p-4">
+        return <div key={item.id} className="flex flex-col bg-gray-700 border rounded-xl space-y-8 w-[50%] text-center m-auto p-4">
             <p className="text-white text-xl">{item.title}</p>
-            <p className="text-white text-sm">{item.description}</p>
-            <div className="flex flex-row text-center gap-12 justify-center">
+            <p className="text-white text-sm line-clamp-5">{item.description}</p>
+            <div className="flex flex-row text-center gap-3 justify-left flex-wrap">
               {item.stack.map((stack, index) => (
-                <span key={index} className="bg-blue-900 text-white p-2 rounded-2xl">{stack}</span>
+                <span key={index} className="bg-blue-800 text-xs text-white p-2 rounded-2xl">{stack}</span>
               ))}
-            </div>    
+            </div>
+            <span className="text-right justify-right">
+              {
+                <a className="bg-black text-white px-4 py-1 rounded-lg cursor-pointer" href={item.url} target="__blank">Visit</a>
+              }
+            </span>    
         </div>
       })}
     </div>
