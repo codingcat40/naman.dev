d637f434aa0f71978824852d8746ac73391ebe06
Naman Matoliya <naman@fieldproxy.com>
2025-12-26 23:12:32 +0530

    projects

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -5,7 +5,7 @@ function Projects() {
     <div className="flex flex-col justify-between pt-24 pb-12 text-center gap-6 font-mono">
       {projects.map((item) => {
         return <div key={item.id} className="flex flex-col bg-[#2A2929] border rounded-xl space-y-8 w-full max-w-lg sm:max-w-xl md:max-w-3xl lg:max-w-5xl mx-auto text-center m-auto p-2">
-            <p className="text-white text-2xl">{item.title}</p>
+            <p className="text-white text-xl text-left font-bold">{item.title}</p>
             <p className="text-white text-lg line-clamp-5">{item.description}</p>
             <div className="flex flex-row text-center gap-3 justify-left flex-wrap">
               {item.stack.map((stack, index) => (
