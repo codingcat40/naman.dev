438c4b228a9e96e6ed3d4531988a31e3895662ca
Naman Matoliya <naman@fieldproxy.com>
2025-12-30 12:54:35 +0530

    commit

--- src/Components/AboutMe.tsx
+++ src/Components/AboutMe.tsx
@@ -63,7 +63,7 @@ const AboutMe = () => {
             {paragraphId === "Intro" ? " Copied! " : " [Share] "}{" "}
           </button>{" "}
           Hello, Stranger!{" "}
-          <span className="text-blue-600">
+          <span className="text-[#5826F2]">
             My Name is Naman Matoliya, I was born in 2001 in India{" "}
           </span>
           As of now while up skilling myself I am looking for an internship /
@@ -112,7 +112,7 @@ const AboutMe = () => {
             {paragraphId === "background" ? " Copied! " : " [Share] "}
           </button>{" "}
           I graduated from school in 2018 and took a gap year for JEE
-          preparation. I started my college journey in <span className="text-blue-600">Computer Science and
+          preparation. I started my college journey in <span className="text-[#5862F2]">Computer Science and
           Engineering in KLE Technological University </span>where I gained the experience of writing code and solving
           real life problems. I worked in the projects in the domains of AI and
           Web development. I also gained hands on experience in these both
@@ -126,8 +126,45 @@ const AboutMe = () => {
       </div>
 
 
+      <div className="flex flex-col mt-4 gap-6">
+          <p id="stack" className="text-left text-2xl text-red-600">My Stack <span className="text-white">
+              <button
+            className="cursor-pointer hover:text-red-500 duration-150"
+            onClick={copyUrl}
+          >
+            {paragraphId === "stack" ? " Copied! " : " [Share] "}
+          </button>{" "}
+            </span></p>
 
-      
+          <p className="text-white text-left text-lg">
+            when developing projects I use different technologies and programming languages,
+            so that the product came out as best as possible and satisfied my desires.
+          </p>
+
+          <p className="text-left text-xl text-red-500">Languages</p>
+          <div className="flex gap-4 text-[#5862F2] text-lg">
+            <p>TypeScript</p>
+            <p>JavaScript</p>
+            <p>Python</p>
+            <p>C++</p>
+          </div>
+
+          <p className="text-left text-xl text-red-500">Technologies</p>
+          <div className="flex-col text-left gap-4 text-[#5862F2] text-lg">
+            <p className="text-white">Frameworks/Libraries:  <span className="text-[#5862F2]">TailwindCSS, ReactJS, NextJS, Express</span></p>
+            <p className="text-white">Development Tools:  </p> <span className="text-[#5862F2]">Vite, VS code</span>
+            <p className="text-white">DataBases</p> <span className="text-[#5862F2]">MongoDB, MySQL</span>
+            <p className="text-white">Version Control Systems:  </p> <span className="text-[#5862F2]">Git, GitHub,  GitLab</span>
+            <p className="text-white">AI  </p> <span className="text-[#5862F2]">Numpy, Pandas, Matplotlib, Scikit learn, Machine Learning</span>
+
+            
+          </div>
+      </div>
+
+    <div className="flex-col text-left">
+      <p className="text-xl text-red-500">Socials <span className="text-white"> [Share]</span> </p>
+
+    </div>
     </div>
   );
 };
