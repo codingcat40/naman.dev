7c97d97ca01747e8610b9c6d58463f7dfd6e4bae
Naman Matoliya <naman@fieldproxy.com>
2026-05-02 12:20:32 +0530

    more projects

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -11,7 +11,7 @@ export const projects = [
     id: 2,
     title: "Memora AI",
     description:
-      "Currently Building Memora AI, a memory-augmented LLM platform using PineCone vector database and semantic retrieval to persist user context across conversations. Implemented structured fact extraction, long-term memory storage, and RAG embedding with personalization. Designed a model-agnostic architecture supporting GPT4, Gemini, DeepSeek, and LLaMA via OpenRouter.",
+      "Built Memora AI, a memory-augmented LLM platform using PineCone vector database and semantic retrieval to persist user context across conversations. Implemented structured fact extraction, long-term memory storage, and RAG embedding with personalization. Designed a model-agnostic architecture supporting GPT4, Gemini, DeepSeek, and LLaMA via OpenRouter.",
 
     stack: [
       "React",
@@ -26,6 +26,14 @@ export const projects = [
 
   {
     id: 3,
+    title: 'GameNews',
+    descrtiption: 'Worked on the GameNews app which renders the latest Games trending in the world. Added the visually appealing styles using three.js library. Implemented Loading, Pagination, Debouncing for Search function and individual game route using the REST API'
+    ,stack: ['React', 'three.js', 'Node', 'Express', 'Discord API'],
+    url: 'https://github.com/codingcat40/GameNews'
+  },
+
+  {
+    id: 4,
     title: "SwoleNORMOUS fitness app",
     description:
       "Developed a fitness app that generates personalized routines—muscle-focused, bro splits, upper/lower splits, and strength/cardio/muscle-gain programs. Includes detailed exercise guidance with reps, rest times, and a clean, mobile-responsive UI.",
@@ -34,7 +42,7 @@ export const projects = [
   },
 
   {
-    id: 4,
+    id: 5,
     title: "MyBlog",
     description: `A simple Blog Application built with the MERN stack (MongoDB, Express, React, Node.js).
 Users can register, log in, create posts, edit posts, and delete posts. Posts are displayed with pagination on the home page.
@@ -42,4 +50,14 @@ Users can register, log in, create posts, edit posts, and delete posts. Posts ar
     stack: ["ReactJS", "Node JS", "MongoDB", "JWT", "Express"],
     url: "https://personal-blog2025.netlify.app/",
   },
+
+
+  {
+    id: 6,
+    title: 'SnapRecipe AI',
+    description: `This is an AI powered Recipe scanner which generates the step by step cooking procedure, list of ingredients once a food picture is uploaded. The images can be in the format of jpg, png and webp`
+,
+stack: ['NextJS', 'TailwindCSS', 'Typescript'],
+url: 'https://snaprecipe-snowy.vercel.app/'
+  }
 ];
